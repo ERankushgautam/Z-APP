9aa9084c20096cb0cae6daff405015e905659d9a
ERhoneydhiman <mhoneyhiman@gmail.com>
2024-07-01 14:03:20 +0530

    signup API

---
+++ backend/db/config.js
@@ -0,0 +1,4 @@
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://zapphd:zapp123@clusterzapp.qfsiyms.mongodb.net/Users?retryWrites=true&w=majority&appName=ClusterZAPP"
+);
---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+require("dotenv").config();
+require("./db/config");
+const User = require("./db/User");
+
+const port = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("hello server");
+});
+
+app.post("/signup", async (req, res) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  result = result.toObject();
+  res.send(result);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
