d8f43cd0fbd776f759404576dbbc6d8f881180c5
ERhoneydhiman <mhoneyhiman@gmail.com>
2024-07-02 12:55:20 +0530

    login api

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("hello server");
+  res.send("hello server!!!!");
 });
 
 // signup API
@@ -21,6 +21,22 @@ app.post("/signup", async (req, res) => {
   let result = await user.save();
   result = result.toObject();
   res.send(result);
+});
+
+// login API
+app.post("/login", async (req, res) => {
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      res.send(user);
+    } else {
+      res.send({ error: "no user with these details" });
+    }
+  } else if (!req.body.password || !req.body.email) {
+    res.send({ error: "give required detail" });
+  } else {
+    res.send({ error: "error" });
+  }
 });
 
 app.listen(port, () => {
